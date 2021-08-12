518f3b5dbeeaa7ee169b5f315a05c19d684f9cc6
Rashaduzamman Rian <rian.acca@gmail.com>
2021-08-12 19:09:56 +0600

    final-1

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const _ = require("lodash");
 const path = require("path");
 
 const app = express();
-// app.use(express.static("../eas_client/build"));
+app.use(express.static("../lucky_strike_trade_client/build"));
 app.use(cors());
 app.use(bodyParser.urlencoded({ extended: false, limit: "5000mb" }));
 app.use(bodyParser.json({ limit: "5000mb" }));
@@ -15,9 +15,9 @@ const port = 5006;
 
 const MongoClient = require("mongodb").MongoClient;
 // const MongoClient = require("mongodb").MongoClient;
-// const uri = "mongodb://127.0.0.1:27017/luckyStrick_Trade";
-const uri =
-  "mongodb+srv://aktcl:01939773554op5t@cluster0.9akoo.mongodb.net/luckyStrick_Trade?retryWrites=true&w=majority";
+const uri = "mongodb://127.0.0.1:27017/luckyStrick_Trade";
+// const uri =
+//   "mongodb+srv://aktcl:01939773554op5t@cluster0.9akoo.mongodb.net/luckyStrick_Trade?retryWrites=true&w=majority";
 const client = new MongoClient(uri, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
@@ -393,9 +393,11 @@ client.connect((err) => {
       console.log(error);
     }
   });
-  // app.get("/", (req, res) => {
-  //   res.send("Connected");
-  // });
+  app.get("*", (req, res) => {
+    res.sendFile(
+      path.join(__dirname, "../lucky_strike_trade_client/build", "index.html")
+    );
+  });
 });
 
 app.get("/", (req, res) => {
